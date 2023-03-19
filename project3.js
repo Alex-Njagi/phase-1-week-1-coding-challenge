d10f3b29c80888cbb349a177c047c8b641943357
Alex Njagi <1992anjagi@gmail.com>
2023-03-19 18:43:05 +0300

    Commenting completed

--- project3.js
+++ project3.js
@@ -1,20 +1,22 @@
 let taxationYear = 2023;
+//Assigns the current year of taxation a variable
 
-let salaryType = window.prompt(`Is the salary monthly or yearly?`);
+let salaryType = window.prompt(`Is the salary monthly or yearly?`);     //Creates a variable that prompts for the user's salary type
     if (salaryType === `Yearly`){
         let salaryType = `Yearly`;
     } else if (salaryType === `Monthly`){
         let salaryType = `Monthly`;
-    } else document.write(`Invalid; Refresh to try again`);
+    }                                                                   //Validates that the salary type is either monthly or yearly
 
-let grossIncome = window.prompt(`Input the ${salaryType} income`);
+let grossIncome = window.prompt(`Input the ${salaryType} income`);      //Creates a variable that prompts the user to input their salary
 let incomeTax;
+//Assigns the income tax a variable
 
-function monthlyIncomeTax(taxableIncome){
+function monthlyIncomeTax(taxableIncome){                               //Creates a function that calculates income tax on a monthly basis
     let firstBracket = .1 * 24000;
     let secondBracket = .25 * (32333-24000);
     let thirdBracket = .3 * (grossIncome - 32333);
-
+    //Creates variables that calculate tax according to the tax brackets
     if (taxableIncome>=11000 && taxableIncome<=24000){
         incomeTax = firstBracket;
     } else if (taxableIncome>24000 && taxableIncome<=32333){
@@ -22,13 +24,14 @@ function monthlyIncomeTax(taxableIncome){
     } else if (taxableIncome>32333){
         incomeTax = firstBracket + secondBracket + thirdBracket;
     }
+    //Assigns the income tax a value based on the tax brackets
 }
 
-function yearlyIncomeTax(taxableIncome){
+function yearlyIncomeTax(taxableIncome){                                //Creates a function that calculates income tax on a monthly basis
     let firstBracket = .1 * 288000;
     let secondBracket = .25 * (388000-288000);
     let thirdBracket = .3 * (grossIncome - 388000);
-
+    //Creates variables that calculate tax according to the tax brackets
     if (taxableIncome>=11000 && taxableIncome<=288000){
         incomeTax = firstBracket;
     } else if (taxableIncome>288000 && taxableIncome<=388000){
@@ -36,28 +39,29 @@ function yearlyIncomeTax(taxableIncome){
     } else if (taxableIncome>388000){
         incomeTax = firstBracket + secondBracket + thirdBracket;
     }
+    //Assigns the income tax a value based on the tax brackets
 }
 
 if (salaryType === `Monthly`){
-    monthlyIncomeTax(taxableIncome = grossIncome);
+    monthlyIncomeTax(taxableIncome = grossIncome);                      //Invokes the monthlyIncomeTax function
 } else if (salaryType === `Yearly`){
-    yearlyIncomeTax(taxableIncome = grossIncome);
+    yearlyIncomeTax(taxableIncome = grossIncome);                       //Invokes the yearlyIncomeTax function
 }
 
-
 let benefitsAsk = window.prompt(`Does the user have NHIF or NSSF or both?`);
-    if (benefitsAsk === `NHIF`){
+//Creates a variable that prompts for the user's benefits
+    if (benefitsAsk === `NHIF`){                        //Validates whether the user has NHIF
         let benefitsAsk = `NHIF`;
-    } else if (benefitsAsk === `NSSF`){
+    } else if (benefitsAsk === `NSSF`){                 //Validates whether the user has NSSF
         let benefitsAsk = `NSSF`;
-    } else if (benefitsAsk === `Both`){
+    } /*else if (benefitsAsk === `Both`){
         let benefitsAsk = `Both`
-    }
-let NHIF;
-let NSSF;
-let totalBenefits;
+    }*/
+let NHIF;                                               //Assigns the NHIF value a variable
+let NSSF;                                               //Assigns the NSSF value a variable
+let totalBenefits;                                      //Assigns the user's benefits' value a variable
 
-function NHIFCalculator(grossIncome){
+function NHIFCalculator(grossIncome){                   //Creates a function for calculating the NHIF benefits
     if (grossIncome>0 && grossIncome<6000){
         NHIF = 150;
     } else if (grossIncome>=6000 && grossIncome<8000){
@@ -93,31 +97,33 @@ function NHIFCalculator(grossIncome){
     } else if (grossIncome>=100000){
         NHIF = 1700;
     }
-    totalBenefits = NHIF;
+    totalBenefits = NHIF;                               //Assigns the total benefits the NHIF benefits value
 }
 
-function NSSFCalculator(grossIncome){
+function NSSFCalculator(grossIncome){                   //Creates a function for calculating the NHIF benefits
     NSSF = .06 * grossIncome;
-    totalBenefits = NSSF;
+    totalBenefits = NSSF;                               //Assigns the total benefits the NSSF benefits value
 }
 
 if (benefitsAsk === `NHIF`){
-    NHIFCalculator(grossIncome=grossIncome);
+    NHIFCalculator(grossIncome=grossIncome);            //Invokes the NHIFCalculator function
 } else if (benefitsAsk === `NSSF`){
-    NSSFCalculator(grossIncome=grossIncome);
+    NSSFCalculator(grossIncome=grossIncome);            //Invokes the NSSFCalculator function
 } /*else if (benefitsAsk === `Both`){
     totalBenefits = NHIF+NSSF;
 }*/
 
 let netSalary = grossIncome - incomeTax - totalBenefits;
+//Calculates the user's net income
 
-function output (){
-    document.write(`Taxation year: ${taxationYear}`);
-    document.write(`;    Salary type: ${salaryType}`);
-    document.write(`;    Gross income: ${grossIncome}`);
-    document.write(`;    Income tax: ${incomeTax}`);
-    document.write(`;    Benefits (NHIF/NSSF): ${benefitsAsk}`);
-    document.write(`;    Total amount of benefits: ${totalBenefits}`);
-    document.write(`;    Net income: ${netSalary}`);
+function output (){                                                     //Creates a function for the final outputs
+    document.write(`Taxation year: ${taxationYear}`);                   //Output the year of taxation
+    document.write(`;    Salary type: ${salaryType}`);                  //Output the salary type
+    document.write(`;    Gross income: ${grossIncome}`);                //Output the gross income
+    document.write(`;    Income tax: ${incomeTax}`);                    //Output the income tax
+    document.write(`;    Benefits (NHIF/NSSF): ${benefitsAsk}`);        //Output the user's benefits
+    document.write(`;    Total amount of benefits: ${totalBenefits}`);  //Output the benefits' value
+    document.write(`;    Net income: ${netSalary}`);                    //Output the user's net income
 }
 output();
+//Invoke the final output function
